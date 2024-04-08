615b991f1216a1a13d4753fb6c63290b71a15ba6
Luka <luka.lefebvrepro@gmail.com>
2024-04-08 11:09:47 +0200

    Done

--- browserRooter/src/App.jsx
+++ browserRooter/src/App.jsx
@@ -1,19 +1,32 @@
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import { Routes, Route, Link } from "react-router-dom";
+import { NavLink, Routes, Route, Link } from "react-router-dom";
 import Page1 from "./Pages/page1";
 import Page2 from "./Pages/page2";
 
 function App() {
   return (
     <>
-      <Link to={"/"}>Page1</Link>
-      <Link to={"/page2"}>Page2</Link>
+      <NavLink
+        style={({ isActive }) => {
+          return isActive ? { color: "red" } : {};
+        }}
+        to={"/"}
+      >
+        Page1
+      </NavLink>
+      <NavLink
+        style={({ isActive }) => {
+          return isActive ? { color: "red" } : {};
+        }}
+        to={"/page2"}
+      >
+        Page2
+      </NavLink>
 
       <Routes>
         <Route path={"/"} element={<Page1 />} />
-
         <Route path={"/page2"} element={<Page2 />} />
       </Routes>
     </>

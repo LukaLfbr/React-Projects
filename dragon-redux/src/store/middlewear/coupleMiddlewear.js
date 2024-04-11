cf567f152ca93bb5beef84af37d3ddac75cdb349
Luka <luka.lefebvrepro@gmail.com>
2024-04-11 09:50:04 +0200

    sync

--- dragon-redux/src/store/middlewear/coupleMiddlewear.js
+++ dragon-redux/src/store/middlewear/coupleMiddlewear.js
@@ -1,30 +1,30 @@
-import { ADD_COUPLE, addCouple } from "../store/reducer/coupleReducer";
-import { setDragonError, setKnightError } from "../store/action/index";
+    import { ADD_COUPLE, addCouple } from "../store/reducer/coupleReducer";
+    import { setDragonError, setKnightError } from "../store/action/index";
 
-const coupleMiddleware = (store) => (next) => (action) => {
-  if (action.type === ADD_COUPLE) {
-    const { dragonId, knightId } = action.payload;
-    const { couples } = store.getState().coupleReducer;
+    const coupleMiddleware = (store) => (next) => (action) => {
+    if (action.type === ADD_COUPLE) {
+        const { dragonId, knightId } = action.payload;
+        const { couples } = store.getState().coupleReducer;
 
-    const isDragonInCouple = couples.some(
-      (couple) => couple.dragonId === dragonId
-    );
-    const isKnightInCouple = couples.some(
-      (couple) => couple.knightId === knightId
-    );
+        const isDragonInCouple = couples.some(
+        (couple) => couple.dragonId === dragonId
+        );
+        const isKnightInCouple = couples.some(
+        (couple) => couple.knightId === knightId
+        );
 
-    if (isDragonInCouple) {
-      store.dispatch(setDragonError("This dragon is already in a couple."));
-      return;
+        if (isDragonInCouple) {
+        store.dispatch(setDragonError("This dragon is already in a couple."));
+        return;
+        }
+
+        if (isKnightInCouple) {
+        store.dispatch(setKnightError("This knight is already in a couple."));
+        return;
+        }
     }
 
-    if (isKnightInCouple) {
-      store.dispatch(setKnightError("This knight is already in a couple."));
-      return;
-    }
-  }
+    return next(action);
+    };
 
-  return next(action);
-};
-
-export default coupleMiddleware;
+    export default coupleMiddleware;
